a9373ba364aa44ea51a7acf77528407f24cad72b
Esteban Rodríguez Alonso <eralonso89@gmail.com>
2020-04-13 18:44:53 -0500

    First commit

---
+++ Firebase/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  SwiftUI-Firebase
+//
+//  Created by Esteban Rodríguez Alonso on 13/04/20.
+//  Copyright © 2020 Esteban Rodríguez Alonso. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "Firebase.h"
+
+#endif /* Bridging_Header_h */
---
+++ SwiftUI-Firebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Firebase
+//
+//  Created by Esteban Rodríguez Alonso on 13/04/20.
+//  Copyright © 2020 Esteban Rodríguez Alonso. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
